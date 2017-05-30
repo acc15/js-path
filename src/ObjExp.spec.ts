4d767d26528b41c3bb37edb3d20c0a70db5bb78d
acc15 <vyacheslav.mayorov@gmail.com>
2017-05-31 00:02:13 +0300

    ObjExp: removed temp spec

--- src/ObjExp.spec.ts
+++ src/ObjExp.spec.ts
@@ -1,11 +1,5 @@
 import "mocha";
-import { expect } from "chai";
-import ObjExp from './ObjExp';
 
 describe("ObjPath", () => {
-    it("ctor must store parts", () => {
-        const parts =  ["a", 1, -1, "b", ""];
-        expect(new ObjExp(parts).parts).eq(parts);
-    });
 
 });
