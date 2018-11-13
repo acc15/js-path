f5501327266c3ade757c9f91f97185014672fc9f
acc15 <vyacheslav.mayorov@gmail.com>
2018-11-13 21:56:12 +0300

    typescript

---
+++ src/Path.spec.ts
@@ -0,0 +1,241 @@
+import {expect} from 'chai';
+import Path, {PathElement} from "./Path";
+
+describe("Path", () => {
+
+    describe("ctor", () => {
+        it("must create from elements", () => {
+
+            const a: Array<PathElement> = [Path.field("test"), Path.index(1), Path.field("abc")];
+            const p = new Path(a);
+
+            a[0] = a[1];
+            expect(p.at(0)).to.be.eq(p.at(1));
+
+        });
+    });
+
+    describe("toString", () => {
+        it("must correctly generate path string", () => {
+            const e = "a[].b[0].c[3][4]..d";
+            expect(Path.of(e).toString()).to.eq(e);
+        });
+    });
+
+    describe("toArray", () => {
+        it("must create new elements copy", () => {
+
+            const p = new Path([Path.field("test"), Path.index(1), Path.field("abc")]);
+            const a = p.toArray();
+            a[0] = { item: "failed", index: false };
+
+            expect(p.at(0).item).eq("test");
+
+        });
+    });
+
+    describe("parse", () => {
+        it("must correctly parse path", () => {
+            expect(Path.of("this.is[3].object[4][5].path").toArray()).to.deep.eq([
+                Path.field("this"),
+                Path.field("is"),
+                Path.index(3),
+                Path.field("object"),
+                Path.index(4),
+                Path.index(5),
+                Path.field("path")
+            ]);
+        });
+
+        it("must correctly handle empty elements", () => {
+            expect(Path.of("a..b[0].abc.[].d").toArray()).to.deep.eq([
+                Path.field("a"),
+                Path.field(""),
+                Path.field("b"),
+                Path.index(0),
+                Path.field("abc"),
+                Path.field(""),
+                Path.emptyIndex(),
+                Path.field("d")
+            ]);
+        });
+
+        it("must correctly parse path ending with index", () => {
+            expect(Path.of("a.b[0]").toArray()).to.deep.eq([Path.field("a"), Path.field("b"), Path.index(0)]);
+        });
+
+        it("must correctly parse path starting with index", () => {
+            expect(Path.of("[0].a").toArray()).to.deep.eq([
+               Path.index(0),
+               Path.field("a")
+            ]);
+        });
+
+        it("must correctly parse empty elements at path start", () => {
+            expect(Path.of(".a").toArray()).to.deep.eq([
+                Path.field(""),
+                Path.field("a")
+            ]);
+        });
+
+        it("must return empty path if string is empty", () => {
+            expect(Path.of("").toArray()).to.be.empty;
+        });
+
+    });
+
+    describe("concat", () => {
+        it("must correctly concat two paths", () => {
+            expect(new Path([Path.field("a")]).concat(new Path([Path.index(1)])).toArray()).to.deep.eq([
+                Path.field("a"), Path.index(1)
+            ]);
+        });
+
+        it("must concat path and path elements array", () => {
+            expect(new Path([Path.field("a")]).concat([Path.index(1)]).toArray()).to.deep.eq([
+                Path.field("a"), Path.index(1)
+            ]);
+        });
+
+        it("must concat single path element", () => {
+            expect(new Path([Path.field("a")]).concat(Path.index(0)).toArray()).to.deep.eq([Path.field("a"), Path.index(0)]);
+        });
+
+        it("must return same path if concat element is null", () => {
+            const p = new Path([Path.field("a")]);
+            expect(p.concat(null)).eq(p);
+        });
+
+        it("must return same path if concat element is empty", () => {
+            const p = new Path([Path.field("a")]);
+            expect(p.concat("")).eq(p);
+        });
+
+    });
+
+    describe("get", () => {
+
+        it("must return value expressed by this path", () => {
+            expect(Path.of("a.b[2].c").get({ a: { b: [0,1,{ c: "Hello world" }] } })).eq("Hello world");
+        });
+
+        it("must return undefined if value not exists", () => {
+            expect(Path.of("a.b[2].c").get({ a: {} })).to.be.undefined;
+        });
+
+    });
+
+    describe("set", () => {
+        it("must set value at specified path", () => {
+            const res = Path.of("a.b[2].c").set({}, 123);
+            expect(res).to.deep.eq({a:{b:[,,{c:123}]}});
+        });
+
+        it("must return val if path is empty", () => {
+            expect(Path.empty().set({}, 123)).eq(123);
+        });
+
+        it("must keep root object", () => {
+            const a: Array<any> = [{a:1},2,3];
+            expect(Path.of("[0].a").set(a, 10)).to.deep.eq([{a:10},2,3]);
+            expect(a[0].a).eq(1);
+        });
+
+    });
+
+    describe("first,last,subPath", () => {
+
+        it("subPath must return path with correct offset and length", () => {
+            const p = Path.of("a.b.c.d").subPath(1, 2);
+            expect(p.toArray()).deep.eq([Path.field("b"), Path.field("c")]);
+        });
+
+        it("subPath must compute valid length if specified is greater then allowed", () => {
+            const p = Path.of("a.b.c.d").subPath(1, 10);
+            expect(p.toArray()).deep.eq([Path.field("b"), Path.field("c"), Path.field("d")]);
+        });
+
+        it("subPath must return empty path if length is zero or less", () => {
+            const p = Path.of("a.b.c.d").subPath(1, -1);
+            expect(p.isEmpty()).is.true;
+        });
+
+        it("first must return first element as path", () => {
+            expect(Path.of("[0].a.b.c").first().toArray()).to.deep.eq([Path.index(0)]);
+        });
+
+        it("last must return empty path if path is empty", () => {
+            const lp = Path.empty().last();
+            expect(lp.isEmpty()).to.be.true;
+        });
+
+        it("last must return last element as path", () => {
+            expect(Path.of("[0].a.b.c").last().toArray()).to.deep.eq([Path.field("c")]);
+        });
+
+    });
+
+    describe("subPath", () => {
+        it("must return new sliced Path", () => {
+            const sp = Path.of("a.b.c.d").subPath(1, 2);
+            expect(sp.toArray()).to.deep.eq([Path.field("b"), Path.field("c")]);
+        });
+
+    });
+
+    describe("of", () => {
+        it("must return path if parameter is path already", () => {
+            const p = Path.of("abc");
+            expect(Path.of(p)).eq(p);
+        });
+
+        it("must return path from string", () => {
+            const p = Path.of("a.b.c");
+            expect(p.toArray()).to.deep.eq([Path.field("a"), Path.field("b"), Path.field("c")]);
+        });
+
+        it("must return path from single element", () => {
+            expect(Path.of(Path.field("a")).toArray()).to.deep.eq([Path.field("a")]);
+            expect(Path.of(Path.index(1)).toArray()).to.deep.eq([Path.index(1)]);
+        });
+
+        it("must return path from element array", () => {
+            expect(Path.of([Path.field("a"), Path.index(1)]).toArray()).to.deep.eq([Path.field("a"), Path.index(1)]);
+        });
+
+        it("must return path from string array", () => {
+            expect(Path.of(["a", "[0].b", "c"]).toArray()).to.deep.eq([Path.field("a"), Path.index(0), Path.field("b"), Path.field("c")]);
+        });
+
+        it("must return path from path array", () => {
+            expect(Path.of([Path.of("a"), Path.of("b"), Path.of("c")]).toArray()).to.deep.eq([
+                Path.field("a"),
+                Path.field("b"),
+                Path.field("c")
+            ]);
+        });
+
+        it("must return path from mixed array", () => {
+            expect(Path.of(["a.b[0]", Path.index(5), new Path([Path.field("c"), Path.index(1)])]).toArray()).to.deep.eq([
+                Path.field("a"),
+                Path.field("b"),
+                Path.index(0),
+                Path.index(5),
+                Path.field("c"),
+                Path.index(1)
+            ]);
+        });
+
+        it("must return path from mixed nested arrays", () => {
+            expect(Path.of(["a", ["b", 0, Path.field("c")], 5, new Path([Path.field("d")])]).toArray()).to.deep.eq([
+                Path.field("a"),
+                Path.field("b"),
+                Path.index(0),
+                Path.field("c"),
+                Path.index(5),
+                Path.field("d")
+            ]);
+        });
+    })
+
+});
---
+++ src/Path.ts
@@ -0,0 +1,192 @@
+
+export interface PathElement {
+    index: boolean;
+    item: string | number;
+}
+
+type AnyPath = undefined | null | string | number | PathElement | Path | AnyPathArray;
+interface AnyPathArray extends Array<AnyPath> {}
+
+export default class Path {
+
+    public static index(i: number): PathElement {
+        return { item: i, index: true };
+    }
+
+    public static emptyIndex(): PathElement {
+        return { item: "", index: true };
+    }
+
+    public static field(name: string): PathElement {
+        return { item: name, index: false };
+    }
+
+    public static empty(): Path {
+        return Path.EMPTY;
+    }
+
+    public static of(p: AnyPath): Path {
+        if (p instanceof Path) {
+            return p;
+        }
+
+        const els: Array<PathElement> = [];
+        Path.collectElements(p, els);
+        return els.length === 0 ? Path.EMPTY : new Path(els);
+    }
+
+    private static EMPTY: Path = new Path([]);
+
+    private static makeElObject(el: PathElement, src: any) {
+        return el.index
+            ? (Array.isArray(src) ? [...src] : [])
+            : typeof src === "object" && src ? {...src} : {};
+    }
+
+    private static parseElements(p: string, els: Array<PathElement> = []) {
+        let keepEmpty: boolean = false;
+        let isIndex: boolean = false;
+        let buf: string = "";
+
+        function resetBuf(nextIsIndex: boolean, nextKeepEmpty: boolean) {
+            if (buf !== "" || keepEmpty) {
+                els.push({
+                    index: isIndex,
+                    item: isIndex && buf ? parseInt(buf, 10) : buf
+                });
+                buf = "";
+            }
+            isIndex = nextIsIndex;
+            keepEmpty = nextKeepEmpty;
+        }
+
+        for (let i = 0; i < p.length; i++) {
+            const c = p[i];
+            if (isIndex) {
+                if (c === ']') {
+                    resetBuf(false, false);
+                    continue;
+                }
+            } else {
+                if (c === '[') {
+                    resetBuf(true, true);
+                    continue;
+                }
+                if (c === '.') {
+                    keepEmpty = i === 0 || p[i - 1] !== ']';
+                    resetBuf(false, true);
+                    continue;
+                }
+            }
+            buf += c;
+        }
+        resetBuf(false, false);
+        return els;
+    }
+
+    private static collectElements(p: AnyPath, els: Array<PathElement>) {
+        if (p instanceof Path) {
+            for (let i = p.offset; i < p.offset + p.length; i++) {
+                els.push(p.elements[i]);
+            }
+        } else if (Array.isArray(p)) {
+            p.forEach(e => Path.collectElements(e, els));
+        } else if (typeof p === "object" && p !== null) {
+            els.push(p);
+        } else if (typeof p === "string") {
+            Path.parseElements(p, els);
+        } else if (typeof p === "number") {
+            els.push(Path.index(p));
+        }
+    }
+
+    private readonly elements: Array<PathElement>;
+    private readonly offset: number;
+    private readonly length: number;
+
+    constructor(els: Array<PathElement>, offset: number = 0, length: number = els.length) {
+        this.elements = els;
+        this.offset = offset;
+        this.length = length;
+    }
+
+    public at(i: number): PathElement {
+        return this.elements[this.offset + i];
+    }
+
+    public isEmpty(): boolean {
+        return this.length === 0;
+    }
+
+    public concat(other: AnyPath): Path {
+        const p = Path.of(other);
+        return p.length === 0 ? this : new Path([...this.toArray(), ...p.toArray()]);
+    }
+
+    public first(): Path {
+        return this.subPath(0, 1);
+    }
+
+    public last(): Path {
+        return this.subPath(this.length - 1, 1);
+    }
+
+    public subPath(offset: number, length: number = this.length - offset): Path {
+        offset = Math.max(0, offset);
+        const newOffset = this.offset + offset;
+        const newLength = Math.max(0, Math.min(this.length - offset, length));
+        return newLength ? new Path(this.elements, newOffset, newLength) : Path.empty();
+    }
+
+    public get(root: any): any {
+        for (let i = this.offset; i < this.offset + this.length; i++) {
+            if (root == null || root === undefined) {
+                break;
+            }
+            root = root[this.elements[i].item];
+        }
+        return root;
+    }
+
+    public set(root: any, val: any): void {
+        if (this.isEmpty()) {
+            return val;
+        }
+
+        const res = Path.makeElObject(this.elements[this.offset], root);
+
+        let trg = res;
+        let src = root;
+        for (let i = this.offset; i < this.offset + this.length - 1; i++) {
+            const el = this.elements[i];
+            const nel = this.elements[i + 1];
+            const nsrc = src[el.item];
+            trg[el.item] = Path.makeElObject(nel, nsrc);
+            src = nsrc || src;
+            trg = trg[el.item];
+        }
+        trg[this.elements[this.offset + this.length - 1].item] = val;
+
+        return res;
+    }
+
+    public toString(): string {
+        let str = "";
+        for (let i = this.offset; i < this.offset + this.length; i++) {
+            const e = this.elements[i];
+            if (e.index) {
+                str += `[${e.item}]`;
+                continue;
+            }
+            if (i > 0) {
+                str += ".";
+            }
+            str += e.item;
+        }
+        return str;
+    }
+
+    public toArray(): Array<PathElement> {
+        return this.elements.slice(this.offset, this.offset + this.length);
+    }
+}
